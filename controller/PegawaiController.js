7e40c696f34c3845fee717c4fa2b18f7139e2ab5
debbysa <deby.silvia23@gmail.com>
2019-09-26 22:19:48 +0700

    menambahkan rute show ke server 2 (gaji)

--- controller/PegawaiController.js
+++ controller/PegawaiController.js
@@ -1,35 +1,48 @@
- const Pegawai = require('../models/Pegawai')
+const Pegawai = require("../models/Pegawai");
+const axios = require("axios");
 
- module.exports = {
+module.exports = {
+  index: function(req, res) {
+    Pegawai.findAll().then(function(rows) {
+      res.json(rows);
+    });
+  },
 
-     index: function (req, res) {
-         Pegawai.findAll().then(function (rows) {
-             res.json(rows)
-         })
-     },
+  show: async (req, res) => {
+    console.log("bisa");
+    axios
+      .get("http://localhost:4000/gaji")
+      .then(response => {
+        res.json(response.data);
+        // console.log(res.data);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+  },
 
-     search: function (req, res) {
-         Pegawai.findByPk(req.params.id).then(function (row) {
-             res.json(row)
-         })
-     },
+  search: function(req, res) {
+    Pegawai.findByPk(req.params.id).then(function(row) {
+      res.json(row);
+    });
+  },
 
-     store: function (req, res) {
-         Pegawai.create(req.body).then(function (rows) {
-             res.json(rows)
-         })
-     },
+  store: function(req, res) {
+    Pegawai.create(req.body).then(function(rows) {
+      res.json(rows);
+    });
+  },
 
-     update: function (req, res) {
-         Pegawai.findByPk(req.params.id).then(function (row) {
-             row.update(req.body)
-             res.json(row)
-         })
-     },
-     destroy: function (req, res) {
-         Pegawai.findByPk(req.params.id).then(function (row) {
-             row.destroy()
-             res.json(row)
-         })
-     }
- }
+  update: function(req, res) {
+    Pegawai.findByPk(req.params.id).then(function(row) {
+      row.update(req.body);
+      res.json(row);
+    });
+  },
+  destroy: function(req, res) {
+    Pegawai.findByPk(req.params.id).then(function(row) {
+      row.destroy();
+      res.json(row);
+    });
+  }
+};
